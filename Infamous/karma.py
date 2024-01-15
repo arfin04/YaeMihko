0e2c69ba65756574bf6235c22c068f54fd0cbc04
Pratham <74235427+Geektyper@users.noreply.github.com>
2024-01-15 22:31:46 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -49,7 +49,7 @@ START_BTN = [
     [
         InlineKeyboardButton(text="DETAILS", callback_data="Miko_"),
         InlineKeyboardButton(text="AI", callback_data="ai_handler"),
-        InlineKeyboardButton(text="SUPPORT", callback_data="https://t.me/ThENiGhTStarS"),
+        InlineKeyboardButton(text="SUPPORT", callback_data="SUPPORT_CHAT"),
     ],
     [
         InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
